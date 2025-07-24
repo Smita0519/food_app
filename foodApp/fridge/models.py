2988ba420dfe12ca874e0d09d000fd85217be8e0
smita <smitamhz13@gmail.com>
2025-07-24 14:23:32 +0545

    added logo and fixed nav bar

--- foodApp/fridge/models.py
+++ foodApp/fridge/models.py
@@ -2,7 +2,3 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
-
-class Recipe(models.Model):
-    title = models.CharField(max_length=100)
-    ingredients = models.ManyToManyField(Ingredient)
